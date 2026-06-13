db7e2a36165d586ac8a4feff48fcef9c65896a5b
Владимир Семенов <svova73747576@gmail.com>
2026-06-13 18:59:06 +0300

    Command execution

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using NetCord;
 using NetCord.Gateway;
 using NetCord.Logging;
+using NetCord.Rest;
+using NetCord.Services;
 using NetCord.Services.ApplicationCommands;
 using NeuroCord.Modules;
 
@@ -26,6 +28,24 @@ var services = new ServiceCollection();
 services.AddSingleton(config);
 var serviceProvider = services.BuildServiceProvider();
 
+discordClient.InteractionCreate += async interaction =>
+{
+    if (interaction is not ApplicationCommandInteraction applicationCommandInteraction)
+        return;
+
+    var result = await applicationCommandService.ExecuteAsync(new ApplicationCommandContext(applicationCommandInteraction, discordClient));
+
+    if (result is not IFailResult failResult)
+        return;
+
+    try
+    {
+        await interaction.SendResponseAsync(InteractionCallback.Message(failResult.Message));
+    }
+    catch
+    {
+    }
+};
 
 await discordClient.StartAsync();
 await Task.Delay(-1);
